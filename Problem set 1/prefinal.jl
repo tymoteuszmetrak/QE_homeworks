19a5728b7e620fb4ef4d7141387367cf5f146a5c
jan-hincz <jan.hincz@outlook.com>
2024-10-31 18:34:11 +0100

    3,4,5,5 prim

--- Problem set 1/prefinal.jl
+++ Problem set 1/prefinal.jl
@@ -1,6 +1,6 @@
 using DelimitedFiles, Plots, Statistics #Hincz, Kuba, Mętrak, Szymczak
 
-# Problem 1 
+# Problem 1: Odd or Even
 #a) with validation of integers
 function odd_or_even(n)
     if typeof(n) == Int64
@@ -38,7 +38,7 @@ odd_or_even_2(-3)
 odd_or_even_2(-4)
 odd_or_even_2(4.5) #incorrect
 
-# Problem 2 
+# Problem 2: Boolean operators
 function compare_three(a, b, c)
     if (a > 0 && b > 0 && c > 0)
         println("All numbers are positive")
@@ -53,3 +53,69 @@ compare_three(1, 2, 3) # Output: All numbers are positive
 compare_three(-1, 5, 7) # Output: At least one number is not positive
 compare_three(0, -4, 3) # Output: At least one number is not positive
 compare_three(0, 0, 0) # Output: All numbers are zero
+
+
+# Problem 3: Factorial Calculation Using a Loop
+function my_factorial(n)
+    result = 1
+    for i in 1:n
+        result = result * i
+    end
+    return result 
+end
+
+my_factorial(0) #1
+my_factorial(1) #1
+my_factorial(2) #2
+my_factorial(5) # Output: 120 (because 5! = 1 × 2 × 3 × 4 × 5 = 120)
+my_factorial(7) # Output: 5040 (because 7! = 1 × 2 × 3 × 4 × 5 × 6 × 7 = 5040)
+
+
+# Problem 4: Count Positive Numbers Using a Loop
+function count_positives(arr)
+    counter = 0
+    for i in arr
+        if (i>0)
+            counter = counter + 1
+        end
+    end
+    return counter
+end
+
+count_positives([1, -3, 4, 7, -2, 0]) # Output: 3
+count_positives([-5, -10, 0, 6]) # Output: 1
+
+
+# Problem 5: Plotting Powers of x Using a Loop 
+
+function plot_powers(n)
+    if n>0 && isinteger(n)     
+        power_plot = plot()
+        for i in 1:n
+            powers(x) = x^i
+            plot!(powers, -10:0.2:10, label = "x^$i", linestyle =:dash, linewidth = 3)
+        end
+        xaxis!("x")
+        yaxis!("y")
+        title!("Powers of x")
+        return power_plot
+    else
+        println("You should have provided a POSITIVE INTEGER")
+    end
+end
+
+plot_powers(-3)
+plot_powers(2.34)
+plot_powers(2)
+my_plot = plot_powers(3)
+my_plot
+
+
+# Problem 5 prim: Count Positive Numbers Using Broadcasting
+
+function count_positives_broadcasting(arr)
+    return sum(arr .> 0)
+end
+count_positives_broadcasting([1, -3, 4, 7, -2, 0]) # 3
+count_positives_broadcasting([-5, -10, 0, 6]) # 1
+
